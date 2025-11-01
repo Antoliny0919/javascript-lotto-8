4eda001a2419ba22933b8a614174ab41ae8c0cd9
antoliny0919 <antoliny0919@gmail.com>
2025-11-01 21:05:49 +0900

    refactor: LottoPrinter가 사용하는 상수명 변경

--- src/LottoPrinter.js
+++ src/LottoPrinter.js
@@ -1,6 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import { LOTTO_PRIZE } from './constants/LottoMachine.js';
-import { LOTTO_RESULT_START_MESSAGE, MESSAGES } from './constants/LottoPrinter.js';
+import { LOTTO_RESULT_START_MESSAGES, LOTTO_MATCH_MESSAGES } from './constants/LottoPrinter.js';
 
 class LottoPrinter {
 
@@ -13,11 +13,11 @@ class LottoPrinter {
   }
 
   static printResult(result, rateOfReturn) {
-    Console.print(MESSAGES.MATCH_RESULT_START);
-    Console.print(MESSAGES.MATCH_RESULT_SEPARATOR_LINE);
+    Console.print(LOTTO_RESULT_START_MESSAGES.MATCH_RESULT_START);
+    Console.print(LOTTO_RESULT_START_MESSAGES.MATCH_RESULT_SEPARATOR_LINE);
     const places = Object.keys(result).reverse();
     for (const place of places) {
-      const matchResultMessage = `${LOTTO_RESULT_START_MESSAGE[place]} (${LOTTO_PRIZE[place].toLocaleString()}원) - ${result[place]}개`
+      const matchResultMessage = `${LOTTO_MATCH_MESSAGES[place]} (${LOTTO_PRIZE[place].toLocaleString()}원) - ${result[place]}개`
       Console.print(matchResultMessage);
     }
     Console.print(`총 수익률은 ${rateOfReturn}%입니다.`);
--- src/constants/LottoPrinter.js
+++ src/constants/LottoPrinter.js
@@ -1,9 +1,9 @@
-export const MESSAGES = Object.freeze({
+export const LOTTO_RESULT_START_MESSAGES = Object.freeze({
   MATCH_RESULT_START: '\n당첨 통계',
   MATCH_RESULT_SEPARATOR_LINE: '---',
 });
 
-export const LOTTO_RESULT_START_MESSAGE = Object.freeze({
+export const LOTTO_MATCH_MESSAGES = Object.freeze({
   first: '6개 일치',
   second: '5개 일치, 보너스 볼 일치',
   third: '5개 일치',
